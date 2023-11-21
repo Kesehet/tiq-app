e9ec1f6de62c6a57e502c2bdaea1a17bf2bae237
Kesehet <hamood.siddiqui@gmail.com>
2023-11-21 08:21:26 +0530

    Added new migration ...

--- database/migrations/2023_11_21_023202_create_quizzes_table.php
+++ database/migrations/2023_11_21_023202_create_quizzes_table.php
@@ -13,8 +13,46 @@ return new class extends Migration
      */
     public function up()
     {
+        // Create quizzes table
         Schema::create('quizzes', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+
+        // Create questions table
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('quiz_id')->constrained()->onDelete('cascade');
+            $table->text('question_text');
+            $table->timestamps();
+        });
+
+        // Create translations table
+        Schema::create('translations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('question_id')->constrained()->onDelete('cascade');
+            $table->string('language');
+            $table->text('translated_text');
+            $table->timestamps();
+        });
+
+        // Create options table
+        Schema::create('options', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('question_id')->constrained()->onDelete('cascade');
+            $table->text('option_text');
+            $table->boolean('is_correct');
+            $table->timestamps();
+        });
+
+        // Create translation_options table
+        Schema::create('translation_options', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('option_id')->constrained('options')->onDelete('cascade');
+            $table->string('language');
+            $table->text('translated_text');
             $table->timestamps();
         });
     }
@@ -26,6 +64,10 @@ return new class extends Migration
      */
     public function down()
     {
+        Schema::dropIfExists('translation_options');
+        Schema::dropIfExists('options');
+        Schema::dropIfExists('translations');
+        Schema::dropIfExists('questions');
         Schema::dropIfExists('quizzes');
     }
 };
