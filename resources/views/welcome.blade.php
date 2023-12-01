0fcb607f352160fcdb1a9e44745e1509a441d473
Kesehet <hamood.siddiqui@gmail.com>
2023-12-01 14:44:08 +0530

    Center aligned the images ...

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -30,7 +30,7 @@
             align-items: center;
             justify-content: center;
             flex-direction: column;
-            padding: 0 20px;
+            
             box-sizing: border-box;
             transform: scale(1.1);
             opacity: 0;
