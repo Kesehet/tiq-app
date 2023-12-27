aa15bc029c095b5f349c5ab93c2d74e05a493526
Kesehet <hamood.siddiqui@gmail.com>
2023-12-27 12:50:50 +0530

    fix logn page scale issue

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,6 +3,7 @@
 <head>
     <title>Login Page</title>
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <style>
         body, html {
             height: 100%;
