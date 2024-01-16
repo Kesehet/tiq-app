1ead5224f95bc1a9743d41a343d8316125c1ded5
Kesehet <hamood.siddiqui@gmail.com>
2024-01-16 08:34:23 +0530

    sasdasd

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -83,7 +83,7 @@ class LoginController extends Controller
     
             // Generate a token or a success indicator for your mobile app
             $token = $user->createToken('YourAppToken')->accessToken;
-    
+            dd($token);
             // Redirect to your custom scheme with the token
             return redirect("tiqapp://login/google/callback?token=$token");
     
