55d713cb859edde40db99ddee339225d9778a8b2
Kesehet <hamood.siddiqui@gmail.com>
2024-01-18 09:35:07 +0530

    commited new url ...

--- resources/views/auth/mobile.blade.php
+++ resources/views/auth/mobile.blade.php
@@ -1,1 +1,6 @@
-<a href="tiq-app://tiq.3duverse.com/login/google/callback?code={{$code}}">Next</a>
+Please continue to the app <a href="tiq-app://tiq.3duverse.com/public/login/google/callback?code={{$code}}">Next</a>
+
+
+<script>
+    window.location.href = "tiq-app://tiq.3duverse.com/public/login/google/callback?code={{$code}}";
+</script>
