ed67303a1bb207d022966f69930081f948aee0a4
Kesehet <hamood.siddiqui@gmail.com>
2024-01-21 09:56:46 +0530

    moved validate to api

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('validate-token', [LoginController::class, 'validateToken'])->name('validateToken');
 
 
 
