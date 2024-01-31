465ac08b9b3e29cf4ac8d688bce54475d2b2f375
Saed Salman <saed.salmann@gmail.com>
2024-02-01 01:43:07 +0530

    i have added a user table

--- resources/views/dashboard/pages/users.blade.php
+++ resources/views/dashboard/pages/users.blade.php
@@ -1,5 +1,15 @@
+<div class="w3-row-padding w3-margin-bottom">
+        <h1>Users</h1>
+    </div>
 
 <table class=" w3-container w3-table w3-striped w3-bordered w3-border w3-hoverable w3-white">
+<tr>
+
+            <th>Name</th>
+            <th>Id</th>
+            <th>Role</th>
+
+        </tr>
 @foreach($Users as $user)
         <tr>
             <td>
